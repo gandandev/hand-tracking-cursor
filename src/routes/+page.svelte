019f63c9fb4ecd21a8ce939e848143383ab7f246
gandan <188059221+gandandev@users.noreply.github.com>
2025-08-23 19:39:31 +0900

    Setup webcam usage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,41 @@
-<div class="w-full h-screen flex flex-col items-center justify-center text-center">
-  <h1 class="font-semibold text-3xl">Hand tracking cursor demo</h1>
-  <p class="text-neutral-500 mt-2">
-    Track your hand movements to control the cursor using computer vision.
-  </p>
-  <button class="bg-neutral-100 rounded-full px-4 py-2 hover:bg-neutral-200 duration-150 active:scale-98 mt-4">Get started</button>
-</div>
+<script lang="ts">
+  let showStartScreen = $state(true)
+  let loadingWebcam = $state(false)
+
+  let videoSource: HTMLVideoElement | null = $state(null)
+
+  async function startWebcam() {
+    try {
+      showStartScreen = false
+      loadingWebcam = true
+
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true
+      })
+      videoSource!.srcObject = stream
+      videoSource!.play()
+
+      loadingWebcam = false
+    } catch (error) {
+      console.log(error)
+    }
+  }
+</script>
+
+{#if showStartScreen}
+  <div class="flex h-screen w-full flex-col items-center justify-center text-center">
+    <h1 class="text-3xl font-semibold">Hand tracking cursor demo</h1>
+    <p class="mt-2 text-neutral-500">
+      Track your hand movements to control the cursor using computer vision.
+    </p>
+    <button
+      class="mt-4 rounded-full bg-neutral-100 px-4 py-2 duration-150 hover:bg-neutral-200 active:scale-98"
+      onclick={startWebcam}
+    >
+      {loadingWebcam ? 'Loading...' : 'Get started'}
+    </button>
+  </div>
+{:else}
+  <!-- svelte-ignore a11y_media_has_caption -->
+  <video bind:this={videoSource} class="h-full w-full -scale-x-100 object-cover"></video>
+{/if}
