71f0004a04b54372284e5da3bbd5ff45e4671ce0
gandan <188059221+gandandev@users.noreply.github.com>
2025-08-23 19:30:34 +0900

    Update landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="w-full h-screen flex flex-col items-center justify-center text-center">
+  <h1 class="font-semibold text-3xl">Hand tracking cursor demo</h1>
+  <p class="text-neutral-500 mt-2">
+    Track your hand movements to control the cursor using computer vision.
+  </p>
+  <button class="bg-neutral-100 rounded-full px-4 py-2 hover:bg-neutral-200 duration-150 active:scale-98 mt-4">Get started</button>
+</div>
