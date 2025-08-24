4182c953030619e64f19e3c2d52369e607be4272
gandan <188059221+gandandev@users.noreply.github.com>
2025-08-24 12:17:48 +0900

    Add transitions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+  import { fly } from 'svelte/transition'
+  import { cubicIn } from 'svelte/easing'
+
   let showStartScreen = $state(true)
-  let loadingWebcam = $state(false)
+  let loadingWebcam = $state(true)
 
   let videoSource: HTMLVideoElement | null = $state(null)
 
@@ -22,20 +25,33 @@
   }
 </script>
 
-{#if showStartScreen}
-  <div class="flex h-screen w-full flex-col items-center justify-center text-center">
-    <h1 class="text-3xl font-semibold">Hand tracking cursor demo</h1>
-    <p class="mt-2 text-neutral-500">
-      Track your hand movements to control the cursor using computer vision.
-    </p>
-    <button
-      class="mt-4 rounded-full bg-neutral-100 px-4 py-2 duration-150 hover:bg-neutral-200 active:scale-98"
-      onclick={startWebcam}
+<div class="relative h-screen w-full">
+  {#if showStartScreen}
+    <div
+      class="absolute flex h-screen w-full flex-col items-center justify-center text-center"
+      transition:fly={{ y: -50, duration: 100, easing: cubicIn }}
     >
-      {loadingWebcam ? 'Loading...' : 'Get started'}
-    </button>
-  </div>
-{:else}
-  <!-- svelte-ignore a11y_media_has_caption -->
-  <video bind:this={videoSource} class="h-full w-full -scale-x-100 object-cover"></video>
-{/if}
+      <h1 class="text-3xl font-semibold">Hand tracking cursor demo</h1>
+      <p class="mt-2 text-neutral-500">
+        Track your hand movements to control the cursor using computer vision.
+      </p>
+      <button
+        class="mt-4 rounded-full bg-neutral-100 px-4 py-2 duration-150 hover:bg-neutral-200 active:scale-98"
+        onclick={startWebcam}
+      >
+        Get started
+      </button>
+    </div>
+  {:else}
+    <!-- svelte-ignore a11y_media_has_caption -->
+    <video
+      bind:this={videoSource}
+      class="absolute h-full w-full -scale-x-100 object-cover duration-1000"
+      class:opacity-0={loadingWebcam}
+      class:blur-lg={loadingWebcam}
+      onplaying={() => {
+        loadingWebcam = false
+      }}
+    ></video>
+  {/if}
+</div>
